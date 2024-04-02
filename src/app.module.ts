01a977952af84886b246da2df2bb355fe9daf300
rahuldahal <rdaahal@gmail.com>
2024-04-02 11:40:27 +0545

    chore: .env and GraphQL setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
+// Nest GraphQL Docs: https://docs.nestjs.com/graphql/quick-start
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // CWD: Current Wo rking Directory
+      // playground: false,  TODO: maybe while in production?
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // .env configuration
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 8888;
+  await app.listen(port);
 }
 bootstrap();
