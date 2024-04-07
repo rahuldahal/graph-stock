fd0b288ce9cb538a697da325bfb21ba3d9c3b1e2
rahuldahal <rdaahal@gmail.com>
2024-04-07 12:20:11 +0545

    chore: config environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,19 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 // Nest GraphQL Docs: https://docs.nestjs.com/graphql/quick-start
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // CWD: Current Wo rking Directory
       // playground: false,  TODO: maybe while in production?
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
